b004dd50181d8254e8b23f10c48f25aff0523ec0
owolfdev <owolfdev@gmail.com>
2023-11-14 04:32:54 +0700

    add api for sb update

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -24,8 +24,37 @@ export async function POST(req: Request) {
   });
 }
 
+export async function PUT(req: Request) {
+  //udate the date in the database
+  await supabase
+    .from("update")
+    .update({
+      date: new Date().toISOString(),
+    })
+    .eq("id", 1);
+
+  const { data } = await supabase.from("update").select("*");
+
+  const responseData = {
+    data,
+  };
+
+  return new Response(JSON.stringify(responseData), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
 export async function GET() {
   const { data } = await supabase.from("update").select("*");
+
+  await supabase
+    .from("update")
+    .update({
+      date: new Date().toISOString(),
+    })
+    .eq("id", 1);
 
   return new Response(`Hello, Next.js! ${JSON.stringify(data)}`);
 }
