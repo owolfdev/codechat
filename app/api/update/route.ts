415b8a2a87fb257f102a513793730b7db01ef99a
owolfdev <owolfdev@gmail.com>
2023-11-14 05:03:03 +0700

    add api for sb update

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -1,28 +1,4 @@
-//make sure this route is allowed to be accessed by the public via the middleware.ts file.
-
 import supabase from "@/lib/supabase-client";
-
-export async function POST(req: Request) {
-  //udate the date in the database
-  await supabase
-    .from("update")
-    .update({
-      date: new Date().toISOString(),
-    })
-    .eq("id", 1);
-
-  const { data } = await supabase.from("update").select("*");
-
-  const responseData = {
-    data,
-  };
-
-  return new Response(JSON.stringify(responseData), {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
 
 export async function PUT(req: Request) {
   //udate the date in the database
